e8bf859befa91820598f2eb9c21d29a139056fe0
ericacswu <55177420+ericacswu@users.noreply.github.com>
2019-12-26 00:48:00 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-lottie.loadAnimation({
-  container: element, // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'data.json' // the path to the animation json
-});
